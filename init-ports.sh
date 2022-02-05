e91a6427a456df3810ac8337dee4fa2f29a1ae5a
Li-Wen Hsu <lwhsu@lwhsu.org>
2022-02-05 09:08:43 +0800

    init-ports: Install ports dev packages

--- init-ports.sh
+++ init-ports.sh
@@ -11,3 +11,8 @@ sudo chown ${USER}:${GROUP} ${PORTS}
 git clone -o freebsd https://git.freebsd.org/ports.git ${PORTS}
 cd ${PORTS}
 git remote set-url --push freebsd git@gitrepo.freebsd.org:ports.git
+
+sudo pkg install -y \
+	portfmt \
+	portlint \
+	py38-python-bugzilla
