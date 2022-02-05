f830c4400d0c0c16ba117251ae3b0fbe30ca0cb5
Li-Wen Hsu <lwhsu@lwhsu.org>
2022-02-05 01:03:13 +0000

    Import

---
+++ init-ports.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+PORTS=/tank/ports
+USER=lwhsu
+GROUP=lwhsu
+
+sudo chown ${USER}:${GROUP} ${PORTS}
+
+git clone -o freebsd https://git.freebsd.org/ports.git ${PORTS}
+cd ${PORTS}
+git remote set-url --push freebsd git@gitrepo.freebsd.org:ports.git
---
+++ init-poudriere.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -ex
+
+if [ `id -u` != 0 ]; then
+	echo "needs root!"
+	exit 1
+fi
+
+RDISK_MPOINT="/mnt/resource"
+
+RDISK=$(mount | grep ${RDISK_MPOINT} | cut -d ' ' -f 1)
+
+if [ -z "${RDISK}" ]; then
+	echo "no resource disk!"
+	exit 1
+fi
+
+umount ${RDISK_MPOINT}
+
+pkg install -y poudriere
+
+zpool create tank ${RDISK}
+zfs create tank/ports
+zfs create tank/distfiles
+
+echo "ALLOW_MAKE_JOBS=yes" >> /usr/local/etc/poudriere.conf
+echo "ZPOOL=tank" >> /usr/local/etc/poudriere.conf
+sed -i.bak -e 's,FREEBSD_HOST=_PROTO_://_CHANGE_THIS_,FREEBSD_HOST=https://download.FreeBSD.org,' /usr/local/etc/poudriere.conf
+sed -i.bak -e 's,DISTFILES_CACHE=/usr/ports/distfiles,DISTFILES_CACHE=/tank/distfiles,' /usr/local/etc/poudriere.conf
+
+poudriere jail -c -j 13_0_amd64 -a amd64 -v 13.0-RELEASE
+poudriere jail -c -j 13_0_i386 -a i386 -v 13.0-RELEASE
+poudriere jail -c -j 12_2_amd64 -a amd64 -v 12.2-RELEASE
+poudriere jail -c -j 12_2_i386 -a i386 -v 12.2-RELEASE
+
+poudriere ports -c -f none -m null -M /tank/ports
